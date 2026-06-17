790b3d0877e80863b6000e19aa01b89d46686e1c
Good Name <dmmedjsoofan@gmail.com>
2026-06-18 03:15:18 +0530

    upgrade portfolio UI

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-alert("Portfolio Loaded Successfully!");
+// Theme toggle
+const btn = document.getElementById("toggleTheme");
+
+btn.addEventListener("click", () => {
+	document.body.classList.toggle("dark");
+});
+
+// Smooth scroll effect
+document.querySelectorAll("nav a").forEach(link => {
+	link.addEventListener("click", function(e) {
+		e.preventDefault();
+		document.querySelector(this.getAttribute("href"))
+		.scrollIntoView({ behavior: "smooth" });
+	});
+});
